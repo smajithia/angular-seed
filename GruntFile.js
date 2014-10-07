a9b98fd8b7d335ca86e47b85e9882af6c456f17a
shruti.majithia <shruti.majithia@bestbuy.com>
2014-10-07 09:14:28 -0500

    Configuring grunt

---
+++ GruntFile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        protractor: {
+            options: {
+                configFile: "protractor.conf.js",
+                keepAlive: true,
+                noColor: false
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                background: true
+            }
+        },
+        watch: {
+            karma: {
+                files: ['app/**/**/*.js'],
+                tasks: ['karma:unit:run']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-protractor-runner');
+
+    grunt.registerTask('devmode', ['karma:unit', 'watch'], 'protractor');
+
+};
